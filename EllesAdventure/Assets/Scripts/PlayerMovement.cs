a037d3529f37ad52dde7e468fc9cac35efb98c87
JamieCarmichael <93764000+JamieCarmichael@users.noreply.github.com>
2023-08-10 08:09:27 +1000

    Update player movement.
    Player movement now maintains speed when turning.

--- EllesAdventure/Assets/Scripts/PlayerMovement.cs
+++ EllesAdventure/Assets/Scripts/PlayerMovement.cs
@@ -20,17 +20,37 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float runAcceleration = 10.0f;
     [Tooltip("The maximum speed the player can move when running.")]
     [SerializeField] private float maxRunSpeed = 10.0f;
-
     /// <summary>
     /// The velocity of the players horizontal movement.
     /// </summary>
     private Vector2 horizontailMovement = Vector2.zero;
+    /// <summary>
+    /// The players current speed.
+    /// </summary>
+    private float speed = 0.0f;
 
     [Header("Rotation")]
     [Tooltip("The players visuals. Rotated to face the players movement direction.")]
     [SerializeField] private Transform playerMesh;
-    [Tooltip("How many seconds it takes for the player to turn 180 degrees.")]
+    [Tooltip("How many seconds it takes for the player to turn to a new movement direction.")]
     [SerializeField] private float rotateSpeed = 10.0f;
+    /// <summary>
+    /// The movement direction before the latest direction.
+    /// </summary>
+    Vector3 fromDirection = Vector3.zero;
+    /// <summary>
+    /// The current direction that the player is moving in.
+    /// </summary>
+    Vector3 toDirection = Vector3.zero;
+    /// <summary>
+    /// True if the mesh is in the pregress of rotating.
+    /// </summary>
+    private bool isRotating = false;
+    /// <summary>
+    /// A timer for how much long the rotation has been going for.
+    /// </summary>
+    private float rotateTimer = 0.0f;
+
 
     [Header("Jump")]
     [Tooltip("How many meters the player can jump.")]
@@ -66,6 +86,9 @@ public class PlayerMovement : MonoBehaviour
         characterController = GetComponent<CharacterController>();
 
         InputManager.Instance.PlayerInput.InGame.Jump.performed += context => Jump();
+
+        fromDirection = playerMesh.forward;
+        toDirection = playerMesh.forward;
     }
 
     private void LateUpdate()
@@ -80,8 +103,9 @@ public class PlayerMovement : MonoBehaviour
     #endregion
 
     #region Private Methods
+
     /// <summary>
-    /// Finds the horizontal velocity for the player.
+    /// Finds the horizontal velocity for the player. Maintains movement speed as long as the player is using an input. Changes direction of movement instantly.
     /// </summary>
     private void Move()
     {
@@ -94,29 +118,76 @@ public class PlayerMovement : MonoBehaviour
         // Get Input
         Vector2 moveInput = InputManager.Instance.PlayerInput.InGame.Move.ReadValue<Vector2>();
         moveInput = moveInput.normalized;
+        bool hasInput = InputManager.Instance.PlayerInput.InGame.Move.IsInProgress();
 
-
-        // Turning
-        Vector3 moveDirection = new Vector3(moveInput.x, 0.0f, moveInput.y).normalized;
-        Vector3 rotationDirection = Vector3.RotateTowards(playerMesh.forward, moveDirection, (1 / rotateSpeed * 2) * Time.deltaTime, 0.0f);
-        playerMesh.rotation = Quaternion.LookRotation(rotationDirection);
-
-        if (!InputManager.Instance.PlayerInput.InGame.Move.IsInProgress())
+        if (!hasInput)
         {
             // No input
-            horizontailMovement = Vector2.MoveTowards(horizontailMovement, Vector2.zero, (maxRunSpeed / deceleration) * Time.deltaTime);
+            speed = Mathf.Clamp(speed - (maxRunSpeed / deceleration * Time.deltaTime), 0, speed);
+            moveInput = horizontailMovement.normalized;
         }
         else if (!InputManager.Instance.PlayerInput.InGame.Sprint.IsInProgress())
         {
             // Walking
-            horizontailMovement = Vector2.MoveTowards(horizontailMovement, moveInput * maxWalkSpeed, (maxWalkSpeed / walkAcceleration) * Time.deltaTime);
+            if (speed > maxWalkSpeed)
+            {
+                speed = Mathf.Clamp(speed - (maxRunSpeed / deceleration * Time.deltaTime), 0, speed);
+            }
+            else
+            {
+                speed = Mathf.Clamp(speed + (maxWalkSpeed / walkAcceleration * Time.deltaTime), 0, maxWalkSpeed);
+            }
         }
         else
         {
             // Running
-            horizontailMovement = Vector2.MoveTowards(horizontailMovement, moveInput * maxRunSpeed, (maxRunSpeed / runAcceleration) * Time.deltaTime);
+            if (speed > maxRunSpeed)
+            {
+                speed = Mathf.Clamp(speed - (maxRunSpeed / deceleration * Time.deltaTime), 0, speed);
+            }
+            else
+            {
+                speed = Mathf.Clamp(speed + (maxRunSpeed / runAcceleration * Time.deltaTime), 0, maxRunSpeed);
+            }
+
+        }
+        horizontailMovement = moveInput * speed;
+
+        // Rotate
+        RotatePlayer(moveInput, hasInput);
+    }
+
+    /// <summary>
+    /// Rotate the player to face the current movement direction.
+    /// </summary>
+    /// <param name="moveInput">The current player input</param>
+    /// <param name="hasInput">If the player is currently making an input</param>
+    private void RotatePlayer(Vector2 moveInput, bool hasInput)
+    {
+        // Turning
+        Vector3 moveInputVector3 = new Vector3(moveInput.x, 0.0f, moveInput.y).normalized;
+        // Check if input has changed.
+        if (toDirection != moveInputVector3 && hasInput)
+        {
+            fromDirection = playerMesh.forward;
+            toDirection = moveInputVector3;
+            rotateTimer = 0.0f;
+            isRotating = true;
+        }
+        // Rotate player if they are not facing the movement direction.
+        if (isRotating)
+        {
+            rotateTimer += Time.deltaTime;
+            Vector3 rotationDirection = Vector3.Slerp(fromDirection, toDirection, rotateTimer / rotateSpeed);
+
+            playerMesh.rotation = Quaternion.LookRotation(rotationDirection);
+            if (rotateTimer >= rotateSpeed)
+            {
+                isRotating = false;
+            }
         }
     }
+
 
     /// <summary>
     /// Checks if the player is grounded. And applies gravity to the players.
@@ -172,6 +243,46 @@ public class PlayerMovement : MonoBehaviour
         if (characterController.collisionFlags == CollisionFlags.Above && verticalVelocity > 0)
         {
             verticalVelocity = 0;
+        }
+    }
+    #endregion
+
+    #region Not In Use
+    /// <summary>
+    /// Finds the horizontal velocity for the player. Sets speed and direction with a single vector that changes to try and match the current input.
+    /// </summary>
+    private void DirectionalMovement()
+    {
+        // If the player is not grounded maintain movement.
+        if (!isGrounded)
+        {
+            return;
+        }
+
+        // Get Input
+        Vector2 moveInput = InputManager.Instance.PlayerInput.InGame.Move.ReadValue<Vector2>();
+        moveInput = moveInput.normalized;
+
+
+        // Turning
+        Vector3 moveDirection = new Vector3(moveInput.x, 0.0f, moveInput.y).normalized;
+        Vector3 rotationDirection = Vector3.RotateTowards(playerMesh.forward, moveDirection, (1 / rotateSpeed * 2) * Time.deltaTime, 0.0f);
+        playerMesh.rotation = Quaternion.LookRotation(rotationDirection);
+
+        if (!InputManager.Instance.PlayerInput.InGame.Move.IsInProgress())
+        {
+            // No input
+            horizontailMovement = Vector2.MoveTowards(horizontailMovement, Vector2.zero, (maxRunSpeed / deceleration) * Time.deltaTime);
+        }
+        else if (!InputManager.Instance.PlayerInput.InGame.Sprint.IsInProgress())
+        {
+            // Walking
+            horizontailMovement = Vector2.MoveTowards(horizontailMovement, moveInput * maxWalkSpeed, (maxWalkSpeed / walkAcceleration) * Time.deltaTime);
+        }
+        else
+        {
+            // Running
+            horizontailMovement = Vector2.MoveTowards(horizontailMovement, moveInput * maxRunSpeed, (maxRunSpeed / runAcceleration) * Time.deltaTime);
         }
     }
     #endregion
